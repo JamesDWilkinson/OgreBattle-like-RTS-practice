2737e47fddaf768e649b04515e60eb5d6bad12af
James Wilkinson <james.ov.wilkinson@gmail.com>
2021-08-28 22:35:09 -0600

    Made a selection group
    Made a GroupSingle in pygame which contains up to one unit for the purposes of Ogre Battle-like unit selection. Updated the methods in the Unit class to add and remove themselves from the "selected" group. This should make it fairly easy to issue commands to units once I figure out how to make multiple distinct units.

--- main.py
+++ main.py
@@ -8,7 +8,18 @@ class Unit(pg.sprite.Sprite):
         self.image = pg.image.load('graphics/Orin03_128x128.png').convert()
         self.rect = self.image.get_rect()
 
+    # Much like in Ogre Battle, we want to be able to select a single unit
+    # No more than one unit, though it might be useful to select multiple
+    # Maybe in the future we can make such a feature,
+    # But it isn't necessary at all for this kind of RTS
+    # Should use a group for selection, thanks, documentation!
+    def deselect(self):
+        self.remove(selected)
+        print('you deselected a unit')
+
     def select(self):
+        self.deselect()
+        self.add(selected)
         print('you selected a unit')
 
     #def update(self):
@@ -45,6 +56,8 @@ unit = Unit()
 unit_group = pg.sprite.Group()
 unit_group.add(unit)
 
+selected = pg.sprite.GroupSingle()
+
 cursor = Cursor()
 cursor_group = pg.sprite.GroupSingle()
 cursor_group.add(cursor)
@@ -59,11 +72,10 @@ while True:
             pg.quit()
             exit()
         if event.type == pg.MOUSEBUTTONDOWN:
-            # Check if the mouse button was pressed while over a unit
-            # Might just want to do this by making a sprite that follows mouse
-            # That way we can use pygame.sprite.spritecollide()? IDK
             if pg.sprite.collide_rect(cursor, unit):
                 unit.select()
+            else:
+                unit.deselect()
 
     # Draw graphics
     screen.fill((25,200,146))
@@ -71,6 +83,8 @@ while True:
     unit_group.update()
     cursor_group.draw(screen)
     cursor_group.update()
+
+    # Print Messages
 
     # Update entire screen
     pg.display.flip()

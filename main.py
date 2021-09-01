f8101a9bcad9beb3a795e86beeceab5de89b9169
James Wilkinson <james.ov.wilkinson@gmail.com>
2021-08-31 21:34:12 -0600

    Can spawn units
    Can spawn units out of unit deployers
    
    currently the coloring is broken: all selectables are red due to their update methods in the Selectable class.
    
    Would like to change the selection graphic to be a green square or circle that goes on top of the sprite
    
    Also should have the units be able to move soon. Vectors woo.

--- main.py
+++ main.py
@@ -1,51 +1,75 @@
 import pygame as pg
 from sys import exit
 
+# Dynamic Groups
+selectables = pg.sprite.Group()
+
+unit_deployers = pg.sprite.Group()
+
+units = pg.sprite.Group()
+
+selected = pg.sprite.GroupSingle()
+
 # Classes
-class Unit_Deployer(pg.sprite.Sprite):
+
+
+class Selectable(pg.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pg.Surface((32,32), pg.SRCALPHA)
+
+        self.add(selectables)
+
+    def select(self):
+        self.add(selected)
+
+    def is_selected(self):
+        if self in selected:
+            self.image.fill('purple')
+        else:
+            self.image.fill('red3')
+
+    def update(self):
+        self.is_selected()
+
+
+class Unit_Deployer(Selectable):
+    def __init__(self):
+        super().__init__()
+
+        self.image = pg.Surface((32, 32), pg.SRCALPHA)
         self.image.fill('red3')
         self.rect = self.image.get_rect()
 
-    def deselect(self):
-        self.remove(selected)
-        print('you deselected a unit deployer')
-        print(selected)
+        self.add(unit_deployers)
+        self.rect.center = pg.mouse.get_pos()
 
-    def select(self):
-        self.add(selected)
-        print('you selected a unit deployer')
-        print(selected)
+    def deploy_unit(self):
+        if self in selected:
+            units.add(Unit((self.rect.x, self.rect.y)))
+
+    def update(self):
+        super().update()
 
 
-class Unit(pg.sprite.Sprite):
-    def __init__(self):
+class Unit(Selectable):
+    def __init__(self, starting_pos):
         super().__init__()
-        self.image = pg.image.load('graphics/Orin03_128x128.png').convert()
-        self.rect = self.image.get_rect()
 
-    def deselect(self):
-        self.remove(selected)
-        print('you deselected a unit')
-        print(selected)
+        self.image = pg.Surface((32, 32), pg.SRCALPHA)
+        self.image.fill('gray')
+        self.rect = self.image.get_rect(center=starting_pos)
 
-    def select(self):
-        self.add(selected)
-        print('you selected a unit')
-        print(selected)
-
-    #def update(self):
+        self.add(units)
 
 
 class Cursor(pg.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pg.Surface((32,32), pg.SRCALPHA)
+        self.image = pg.Surface((32, 32), pg.SRCALPHA)
         self.rect = pg.draw.polygon(
-            self.image, (255, 255, 255), [(0,0),(16,32),(32,16)]
-            )
+            self.image, (255, 255, 255), [(0, 0), (16, 32), (32, 16)]
+        )
+        self.rect = pg.Rect(0, 0, 1, 1)
 
     def update(self):
         self.rect.topleft = pg.mouse.get_pos()
@@ -58,22 +82,13 @@ clock = pg.time.Clock()
 
 screen_width = 800
 screen_height = 400
-screen = pg.display.set_mode((screen_width,screen_height))
+screen = pg.display.set_mode((screen_width, screen_height))
 
 pg.display.set_caption('OB_like')
 
 pg.mouse.set_visible(False)
 
-# Groups
-
-unit_deployers = pg.sprite.Group()
-
-units = pg.sprite.Group()
-
-selectables = pg.sprite.Group()
-
-selected = pg.sprite.GroupSingle()
-
+# Static Groups
 cursor = Cursor()
 cursor_group = pg.sprite.GroupSingle()
 cursor_group.add(cursor)
@@ -82,22 +97,30 @@ cursor_group.add(cursor)
 
 while True:
     # Main event loop
-    
+
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
             exit()
         if event.type == pg.MOUSEBUTTONDOWN:
             if pg.mouse.get_pressed()[0]:
-                print(pg.sprite.spritecollide(cursor, selectables, False))
+                selected.empty()
+                if pg.sprite.spritecollide(cursor, selectables, False):
+                    pg.sprite.spritecollide(
+                        cursor, selectables, False
+                    )[-1].select()
+                print(selected.sprites())
+
+            if pg.mouse.get_pressed()[1]:
+                unit_deployers.add(Unit_Deployer())
 
             if pg.mouse.get_pressed()[2]:
-                unit_deployers.add(Unit_Deployer())
-                unit_deployers.sprites()[-1].rect.center = pg.mouse.get_pos()
-                unit_deployers.sprites()[-1].add(selectables)
-             
+                if selected.sprites():
+                    if selected.sprites()[0] in unit_deployers:
+                        selected.sprites()[0].deploy_unit()
+
     # Draw graphics
-    screen.fill((25,200,146))
+    screen.fill((25, 200, 146))
     unit_deployers.draw(screen)
     unit_deployers.update()
     units.draw(screen)
